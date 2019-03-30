637f7bc7abc1651a842552b0e0a2dbd844fb2998
ospeyer <ospeyer@noa.gr>
2019-03-30 19:46:23 +0200

    Update run.sh and directory paths

--- SOURCE/run.sh
+++ SOURCE/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd SDG_CALC/INPUT
-unzip INPUT_shp.zip
-cd ../
-cd SOURCE
-pwd
+#cd SDG_CALC/INPUT
+unzip ../INPUT/INPUT_shp.zip -d ../INPUT
+#cd ../
+#cd SOURCE
+#pwd
 /opt/conda/envs/nclenv/bin/ncl  CAMS_shapefile.ncl
-
+rm ../INPUT/TR*
